5c828ea8d41fbff6be107e7a64fdac2b32fb1e48
nonfatchobani <khn1205@gmail.com>
2023-04-13 13:39:59 -0500

    update

--- code/file_paths.R
+++
@@ -1,7 +0,0 @@
-# This scripts defines the root directory where files should be read from and saved
-
-root_dir <- file.path("/home/traceymangin/climate-aggregate")
-
-input_dir <- file.path("/home/tcarleton/Climate")
-
-save_dir <- file.path("/home/traceymangin")
